c02d9499e51bbb1947d1d5c7e1144dddb75aad60
shaddoxac <shaddoxac@gmail.com>
2015-04-01 17:17:01 -0500

    Added Setting Bits for real

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,8 +1,8 @@
 package game;
 
-
+//squares go left to right, top to bottom
 public class Game {
-    private int white, black, whiteKing, blackKing;
+    private int whitePos, blackPos, whiteKingPos, blackKingPos;
     private int whiteStartingPos=0xFFF00000;
     private int blackStartingPos=0x00000FFF;
 
@@ -12,13 +12,44 @@ public class Game {
 
 
     private void setBoard() {
-        white=whiteStartingPos;
-        black=blackStartingPos;
-        whiteKing=0;
-        blackKing=0;
+        whitePos=whiteStartingPos;
+        blackPos=blackStartingPos;
+        whiteKingPos=0;
+        blackKingPos=0;
     }
 
-    private boolean checkMove() {
-        return true; //TODO
+    private void changeMove(Piece pieceType, int loc, int dest) {
+        int bitBoard=getBitBoard(pieceType);
+        bitBoard=bitBoard & ~loc;
+        bitBoard=bitBoard | dest;
+        setBitBoard(bitBoard,pieceType);
+    }
+
+    private void setBitBoard(int bitBoard, Piece pieceType) {
+        if (pieceType.equals(Piece.BLACK)) {blackPos=bitBoard;}
+        else if (pieceType.equals(Piece.WHITE)) {whitePos=bitBoard;}
+        else if (pieceType.equals(Piece.BLACKKING)) {blackKingPos=bitBoard;}
+        else {whiteKingPos=bitBoard;}
+    }
+
+    private int getBitBoard(Piece pieceType) {
+        if (pieceType.equals(Piece.BLACK)) {return blackPos;}
+        if (pieceType.equals(Piece.WHITE)) {return whitePos;}
+        if (pieceType.equals(Piece.BLACKKING)) {return blackKingPos;}
+        return whiteKingPos;
+    }
+    private boolean checkMove(int dest) {
+        if (checkOccupied(whitePos, dest) || checkOccupied(blackPos, dest)) {
+            if (checkOccupied(blackKingPos, dest) || checkOccupied(whiteKingPos, dest)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean checkOccupied(int bucket, int dest) {
+        if ((bucket & dest)!=0) {
+            return false;
+        }
+        return true;
     }
 }
