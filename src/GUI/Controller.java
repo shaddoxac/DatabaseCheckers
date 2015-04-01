2f41ed836c7f53bca102edca3a739a2b029f0e66
grempey <empeyge@hendrix.edu>
2015-04-01 18:41:18 -0500

    more GUI

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,5 +1,34 @@
 package GUI;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
+public class Controller {
+	
+	@FXML
+	Button newGame;
+	
+	@FXML
+	TextField numMoves;
+	
+	@FXML
+	ChoiceBox<String> difficulty;
+	
+	@FXML
+	ChoiceBox<String> boardStyle;
+	
+	
+	@FXML
+	private void initialize() {
+		difficulty = new ChoiceBox<String>(FXCollections.observableArrayList(
+			"Easy", "Medium", "Hard")
+		);
+		boardStyle = new ChoiceBox<String>(FXCollections.observableArrayList(
+			"Style1", "Style2", "Style3")
+		);
+		
+	}
 }
--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,17 +1,20 @@
 package GUI;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("CheckerGUI.fxml"));
+			AnchorPane root = (AnchorPane) loader.load();
+			Scene scene = new Scene(root,800,512);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
