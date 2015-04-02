29e0b3241f94e9f0ab43e0ff62c374ff70a7da76
grempey <empeyge@hendrix.edu>
2015-04-01 21:06:53 -0500

    controller and GUI update

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,17 +1,30 @@
 package GUI;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 
 public class Controller {
+	
+	@FXML
+	ImageView board = new ImageView();
 	
 	@FXML
 	Button newGame;
 	
 	@FXML
 	TextField numMoves;
+	
+	@FXML
+	TextField turnIndicator;
 	
 	@FXML
 	ChoiceBox<String> difficultyBox;
@@ -22,15 +35,35 @@ public class Controller {
 	
 	@FXML
 	private void initialize() {
+        turnIndicator.setBackground(new Background(new BackgroundFill(Color.CHOCOLATE, CornerRadii.EMPTY, Insets.EMPTY)));
+        turnIndicator.setStyle("-fx-text-inner-color: black;");
         setUpBoxes();
+        setUpBoard();
+       	}
+
+	private void switchTurns() {
+		String currentTurn = turnIndicator.getText();
+		boolean isPlayerTurn = currentTurn.equals("Your turn");
+		String newTurn = isPlayerTurn ? "CPU turn" : "Your turn";
+		String newColor = isPlayerTurn ? "white" : "black";
+		turnIndicator.setText(newTurn);
+		turnIndicator.setStyle("-fx-text-inner-color: " + newColor + ";");
 	}
-
-
-
+	
     private void setUpBoxes() {
         difficultyBox.getItems().addAll("Easy", "Medium", "Hard");
         difficultyBox.setValue("Medium");
-        boardStyleBox.getItems().addAll("Style1", "Style2", "Style3");
-        boardStyleBox.setValue("Style1");
+        boardStyleBox.getItems().addAll("Style 1", "Style 2", "Style 3"); //TODO: make different board styles
+        boardStyleBox.setValue("Style 1");
+    }
+    
+    private void setUpBoard() {
+    	String boardStyle = boardStyleBox.getValue();
+    	switch (boardStyle) {
+    	case "Style 1":
+    		Image boardImage = new Image(this.getClass().getResourceAsStream("Game_Board.png"));
+        	board.setImage(boardImage);
+    		break;
+    	}
     }
 }
